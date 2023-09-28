2fa1e16def304952c7ddb80884714c25bebbca5f
RobinPICODEV <0x.88ta.ko@gmail.com>
2023-09-28 19:53:30 +0200

    config the windows sizes && declare errors

---
+++ snake.py
@@ -0,0 +1,16 @@
+import pygame, sys, time, random
+
+speed = 15
+
+# windows sizes
+
+frame_sizes_x = 720
+frame_sizes_y = 480
+
+check_errors + pygame.init()
+
+if(check_errors[1] > 0):
+    print("Errors " + check_errors[1])
+else:
+    print("Game Succesfully initialized!")
+    
