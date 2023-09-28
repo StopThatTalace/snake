7d93f564f6d638323524f6cc7a9d20f3768efb29
RobinPICODEV <0x.88ta.ko@gmail.com>
2023-09-28 21:06:56 +0200

    Add head_color

--- snake.py
+++ snake.py
@@ -24,6 +24,7 @@ black = pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
 red = pygame.Color(255,0,0)
 green = pygame.Color(0,255,0)
+lighter_green = pygame.Color(0, 100, 0)
 blue = pygame.Color(0,0,255)
 
 fps_controller = pygame.time.Clock()
@@ -33,7 +34,7 @@ fps_controller = pygame.time.Clock()
 square_size = 20
 
 def init_vars():
-    global head_pos, snake_body, food_pos, food_spawn, score, direction
+    global head_pos, snake_body, food_pos, food_spawn, score, direction, head_color
     direction = "RIGHT"
     head_pos = [120, 60]
     snake_body = [[120, 60]]
@@ -41,6 +42,7 @@ def init_vars():
                 random.randrange(1,(frame_size_y // square_size)) * square_size]
     food_spawn = True
     score = 0
+    head_color = lighter_green
 
 init_vars()
 
@@ -64,17 +66,13 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_UP or event.key == ord("w")
-                and direction != "DOWN"):
+            if (event.key == pygame.K_UP or event.key == ord("w")) and direction != "DOWN":
                 direction = "UP"
-            elif ( event.key == pygame.K_DOWN or event.key == ord("s")
-                and direction != "UP"):
+            elif (event.key == pygame.K_DOWN or event.key == ord("s")) and direction != "UP":
                 direction = "DOWN"
-            elif ( event.key == pygame.K_LEFT or event.key == ord("a")
-                and direction != "RIGHT"):
+            elif (event.key == pygame.K_LEFT or event.key == ord("a")) and direction != "RIGHT":
                 direction = "LEFT"
-            elif ( event.key == pygame.K_RIGHT or event.key == ord("d")
-                and direction != "LEFT"):
+            elif (event.key == pygame.K_RIGHT or event.key == ord("d")) and direction != "LEFT":
                 direction = "RIGHT"
 
     if direction == "UP":
@@ -90,8 +88,8 @@ while True:
         head_pos[0] = frame_size_x - square_size
     elif head_pos[0] > frame_size_x - square_size:
         head_pos[0] = 0
-    elif head_pos[1] < frame_size_y - square_size:
-        head_pos[1] = 0
+    elif head_pos[1] < 0:
+        head_pos[1] = frame_size_y - square_size
     elif head_pos[1] > frame_size_y - square_size:
         head_pos[1] = 0
 
@@ -108,15 +106,26 @@ while True:
     if not food_spawn:
         food_pos = [random.randrange(1, (frame_size_x // square_size)) * square_size,
                     random.randrange(1, (frame_size_y // square_size)) * square_size]
+        food_spawn = True
 
     # GFX
     game_window.fill(black)
     for pos in snake_body:
         pygame.draw.rect(game_window, green, pygame.Rect(
             pos[0] + 2, pos[1] + 2,
-            square_size -2, square_size))
-    pygame.draw.rect(game_window, red, pygame.Rect(food_pos[0],
-                    food_pos[1], square_size, square_size))
+            square_size - 2, square_size))
+        pygame.draw.rect(game_window, red, pygame.Rect(food_pos[0],
+                                                       food_pos[1], square_size, square_size))
+        if pos == head_pos:
+            pygame.draw.rect(game_window, head_color, pygame.Rect(
+                pos[0] + 2, pos[1] + 2,
+                square_size - 2, square_size))
+        else:
+            pygame.draw.rect(game_window, green, pygame.Rect(
+                pos[0] + 2, pos[1] + 2,
+                square_size - 2, square_size))
+
+
 
     # game over confitions
 
